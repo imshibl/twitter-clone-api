2f56467b73741d3087e06f763189054d15dd7cde
Shibl <59617790+imshibl@users.noreply.github.com>
2021-11-23 12:22:12 +0530

    Update database.py

--- app/twitter/database.py
+++ app/twitter/database.py
@@ -2,10 +2,9 @@ from sqlalchemy import create_engine, engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# SQLALCHEMY_DATABASE_URL = "postgresql://postgres:geekinside36@localhost:3636/twitterclonedb"
-SQLALCHEMY_DATABASE_URL = "postgresql://gxrhqrpwbglkcf:3f9e5b26475c94a3ea729e1d06c31b0a9267dbc4f562cba76812d2c612bcb8bc@ec2-54-84-142-90.compute-1.amazonaws.com:5432/dpip3c35l9rhc"
 
-# psql --host=ec2-54-84-142-90.compute-1.amazonaws.com --port=5432 --username=gxrhqrpwbglkcf --password --dbname=dpip3c35l9rhc
+SQLALCHEMY_DATABASE_URL = "" <- add your database. refer "https://fastapi.tiangolo.com/tutorial/sql-databases/"
+
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
